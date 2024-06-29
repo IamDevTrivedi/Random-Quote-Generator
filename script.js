7ec1ee955a4117a14afd51fc596d6e438d171a8d
DEV TRIVEDI <152381039+IamDevTrivedi@users.noreply.github.com>
2024-06-29 22:26:33 +0530

    Files Uploaded

---
+++ script.js
@@ -0,0 +1,46 @@
+const quoteText = document.querySelector('#quote');
+const authorName = document.querySelector('#author');
+
+const body = document.querySelector('body');
+const quoteBox = document.querySelector('#quote-box');
+
+const btn = document.querySelector('button');
+
+async function fetchQuote() {
+    try {
+        const response = await fetch("https://api.quotable.io/random");
+        const quote = await response.json();
+        return quote;
+    } catch (error) {
+        console.error("Error fetching quote: ", error);
+    }
+}
+
+async function displayRandomQuote() {
+    const quote = await fetchQuote();
+    if (quote) {
+        quoteText.textContent = quote.content;
+        authorName.textContent = `- ${quote.author || 'Unknown'}`;
+    }
+
+    // let r = Math.floor(Math.random() * 256);
+    // let g = Math.floor(Math.random() * 256);
+    // let b = Math.floor(Math.random() * 256);
+    // let color1 = `rgb(${r},${g},${b})`;
+    // let color2 = `rgba(${r},${g},${b},0.25)`;
+
+    body.style.backgroundColor = color2;
+    quoteBox.style.backgroundColor = color1;
+}
+
+btn.addEventListener("click", displayRandomQuote);
+
+btn.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        displayRandomQuote();
+        setTimeout(() => {
+        }, 250);
+    }
+});
+
+displayRandomQuote();
